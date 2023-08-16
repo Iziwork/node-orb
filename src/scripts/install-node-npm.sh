26c5b080a9fda3d97c219cb5ed87d648d0c65b15
Guillaume Lecerf <guillaume.lecerf@iziwork.com>
2023-08-16 16:28:05 +0200

    refactor: simplify install-node-npm.sh

--- src/scripts/install-node-npm.sh
+++ src/scripts/install-node-npm.sh
@@ -21,54 +21,48 @@ install_nvm() {
 
 install_node() {
   local pkg_node_version
-  pkg_node_version=$(jq ".engines.node" package.json)
+  pkg_node_version=$(jq -r ".engines.node // empty" package.json)
 
-  if [[ "$pkg_node_version" == "null" ]]; then
+  if [ -z "$pkg_node_version" ]; then
     echo "No engines.node specified, skipping node installation"
     return
   fi
 
-  local pkg_node_version_stripped
-  pkg_node_version_stripped=$(echo "$pkg_node_version" | jq -r .)
-
   if command -v node > /dev/null 2>&1; then
-    if node -v | grep "$pkg_node_version_stripped" > /dev/null 2>&1; then
-      echo "node $pkg_node_version_stripped is already installed"
+    if node -v | grep "$pkg_node_version" > /dev/null 2>&1; then
+      echo "node $pkg_node_version is already installed"
       return
     fi
   fi
 
-  nvm install "$pkg_node_version_stripped"
-  nvm alias default "$pkg_node_version_stripped"
+  nvm install "$pkg_node_version"
+  nvm alias default "$pkg_node_version"
   echo 'nvm use default &>/dev/null' >> "$BASH_ENV"
 }
 
 install_npm() {
   local pkg_npm_version
-  pkg_npm_version=$(jq ".engines.npm" package.json)
+  pkg_npm_version=$(jq -r ".engines.npm // empty" package.json)
 
-  if [[ "$pkg_npm_version" == "null" ]]; then
+  if [ -z "$pkg_npm_version" ]; then
     echo "No engines.npm specified, skipping npm installation"
     return
   fi
 
-  local pkg_npm_version_stripped
-  pkg_npm_version_stripped=$(echo "$pkg_npm_version" | jq -r .)
-
-  if [[ "$pkg_npm_version_stripped" == "please-use-yarn" ]]; then
+  if [ "$pkg_npm_version" = "please-use-yarn" ]; then
     echo "please-use-yarn marker found, skipping npm installation"
     return
   fi
 
 
   if command -v npm > /dev/null 2>&1; then
-    if npm -v | grep "$pkg_npm_version_stripped" > /dev/null 2>&1; then
-      echo "npm $pkg_npm_version_stripped is already installed"
+    if npm -v | grep "$pkg_npm_version" > /dev/null 2>&1; then
+      echo "npm $pkg_npm_version is already installed"
       return
     fi
   fi
 
-  npm install -g "npm@$pkg_npm_version_stripped"
+  npm install -g "npm@$pkg_npm_version"
 }
 
 install_corepack() {
@@ -82,7 +76,7 @@ install_corepack() {
 
 # Will not run if sourced for bats-core tests.
 ORB_TEST_ENV="bats-core"
-if [ "${0#*"$ORB_TEST_ENV"}" == "$0" ]; then
+if [ "${0#*"$ORB_TEST_ENV"}" = "$0" ]; then
   install_nvm
   install_node
   install_npm
